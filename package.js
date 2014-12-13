78f97f01cff4f0d4b4774b1e5cc1e195b3113f92
Roel <roel@ralphstarck.com>
2014-12-13 14:46:39 +0700

    Add jquery dependency

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
 Package.describe({
-  name: 'jquery-readmore',
-  summary: 'A smooth, lightweight jQuery plugin for collapsing and expanding long blocks of text with "Read more" and "Close" links.'
+  name: 'rova:jquery-readmore',
+  summary: 'A smooth, lightweight jQuery plugin for collapsing and expanding long blocks of text with "Read more" and "Close" links.',
+  version: '0.0.1',
+  git: 'git@github.com:roelvan/meteor-readmore.git'
 });
 
 Package.on_use(function (api, where) {
